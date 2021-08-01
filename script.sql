d618d6d806211deb9f1792f4a723ec6feeb6aa61
zhangyl <zhangyl@maycur.com>
2021-08-01 12:35:19 +0800

    更新初始化脚本

--- script.sql
+++ script.sql
@@ -13,14 +13,15 @@ CREATE TABLE `cost` (
 ENGINE=InnoDB AUTO_INCREMENT=1;
 
 
--- 以下做一张单表到默认库并插入两条数据试试程序做路由
+-- 以下表在默认数据库必须建立，dynamic-datasource相关。做一张单表到默认库并插入两条数据试试程序做路由
 CREATE TABLE multi_default.`other_datasource` (
 	`id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-	`datasource_name` VARCHAR(64) NOT NULL,
+	`datasource_name` VARCHAR(64) NOT NULL COMMENT '数据源名称。也被用作spring数据源bean名称，路由时候设置的数据源编码，需要保证唯一',
 	`database_ip` VARCHAR(64) NOT NULL,
 	`database_name` VARCHAR(256) NOT NULL,
 	`database_port` VARCHAR(64) NOT NULL,
 	`database_type` INT NOT NULL COMMENT '1: master主库 2: slave库',
+	`router_code` INT NOT NULL COMMENT '路由时候设置的数据源路由编码，主库必须唯一。比如：如果根据租户A的企业编码路由，routerCode=租户A的企业编码',
 	`database_username` VARCHAR(64) NOT NULL,
 	`database_password` VARCHAR(64) NOT NULL,
 	`database_url_extra_param` VARCHAR(512) NOT NULL,
@@ -28,9 +29,9 @@ CREATE TABLE multi_default.`other_datasource` (
 )
 ENGINE=InnoDB AUTO_INCREMENT=1;
 
-INSERT INTO multi_default.`other_datasource` (`id`, `datasource_name`, `database_ip`, `database_name`, `database_port`, `database_username`, `database_password`, `database_url_extra_param`) VALUES
-	(1, 'multi_1', '127.0.0.1', 'multi_1', '3306', 1, 'root', '123456', 'useUnicode=true&characterEncoding=utf-8&serverTimezone=GMT%2B8&useSSL=false'),
-	(2, 'multi_2', '127.0.0.1', 'multi_2', '3306', 1, 'root', '123456', 'useUnicode=true&characterEncoding=utf-8&serverTimezone=GMT%2B8&useSSL=false');
+INSERT INTO multi_default.`other_datasource` (`id`, `datasource_name`, `database_ip`, `database_name`, `database_port`, `database_type`, `router_code`, `database_username`, `database_password`, `database_url_extra_param`) VALUES
+	(1, 'multi_1', '127.0.0.1', 'multi_1', '3306', 1, 'multi_1', 'root', '123456', 'useUnicode=true&characterEncoding=utf-8&serverTimezone=GMT%2B8&useSSL=false'),
+	(2, 'multi_2', '127.0.0.1', 'multi_2', '3306', 1, 'multi_2', 'root', '123456', 'useUnicode=true&characterEncoding=utf-8&serverTimezone=GMT%2B8&useSSL=false');
 	
 	
 
