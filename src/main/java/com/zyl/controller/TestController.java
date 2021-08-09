e6e750846782a34ba0b911bf3e2837d66fe42d36
zhangyl <zhangyl@maycur.com>
2021-08-09 09:05:16 +0800

    重复定义

--- src/main/java/com/zyl/controller/TestController.java
+++ src/main/java/com/zyl/controller/TestController.java
@@ -19,7 +19,7 @@ public class TestController {
 	@Autowired
 	CostService costService;
 	
-	@PostMapping("testAnnotationSlave")
+	@PostMapping("testSelect")
 	public String testSelect(HttpServletRequest request) {
         
         {
